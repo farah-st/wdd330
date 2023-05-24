00abda425d5ff6cedaaed91af4efe52fbe3e8928
farah-st <farah.perezcarrillo@gmail.com>
2023-05-23 18:37:11 -0600

    third reading

--- scripts/week06.js
+++ scripts/week06.js
@@ -33,3 +33,53 @@ addForm.addEventListener('submit', function(e){
   li.appendChild(deleteBtn);
   list.appendChild(li);
 });
+
+//FormData Onject
+document.addEventListener('DOMContentLoaded', () => {
+  document
+    .getElementById('form1')
+    .addEventListener('submit', handleForm);
+});
+
+function handleForm(ev) {
+  ev.preventDefault(); //stop the page reloading
+  //console.dir(ev.target);
+  let form1 = ev.target;
+  let data = new FormData(form1);
+
+  //add more things that were not in the form
+  data.append('api-key', 'myApiKey');
+
+  //look at all the contents
+  for (let key of data.keys()) {
+    console.log(key, data.get(key));
+  }
+  let json = convertdata2JSON(data);
+
+  //send the request with the formdata
+  let url = 'http://localhost:3000/'; //or some URL 
+  let h = new Headers();
+  h.append('Content-type', 'application/json');
+
+  let req = new Request(url, {
+    headers: h,
+    body: json,
+    method: 'POST',
+  });
+  //console.log(req);
+  fetch(req)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Response from server');
+      console.log(data);
+    })
+    .catch(console.warn);
+}
+
+function convertdata2JSON(formData) {
+  let obj = {};
+  for (let key of formData.keys()) {
+    obj[key] = formData.get(key);
+  }
+  return JSON.stringify(obj);
+}
